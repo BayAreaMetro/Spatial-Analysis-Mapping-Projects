d753650dd06722b7b183f81fb16337dc7174c926
jcroff <joshua.croff@gmail.com>
2017-09-12 16:22:25 -0700

    Extract csv summary steps from script

--- Regional-Goods-Movement/scripts/Python/Create_Northern_CA_Megaregion_Businesses_2016_FC.py
+++ Regional-Goods-Movement/scripts/Python/Create_Northern_CA_Megaregion_Businesses_2016_FC.py
@@ -1,4 +1,5 @@
 # This script should be run within the ArcGIS Pro Python Window 
+# Add GMS_2016_CA_Businesses to map before proceeding 
 
 import arcpy
 import pandas as pd 
@@ -74,31 +75,6 @@ scClassSelectionDict = {scProduction:"1",scTransportation:"2",scDistribution:"3"
 
 #Populate Supply_Chain_Class Field
 updateClassField(norCalMegaRegBusinesses, "Supply_Chain_Class", scClassSelectionDict)
-
-#Create function which returns pandas DF
-def feature_class_to_pandas_df(feature_class,field_list):
-	return pd.DataFrame(arcpy.da.FeatureClassToNumPyArray(feature_class,field_list,skip_nulls=False))
-
-#Create pandas df from Northern California 2016 Feature Class
-norCalMegaRegBusinessesDF = feature_class_to_pandas_df(norCalMegaRegBusinesses, ('EMPNUM','Goods_Mvmt_Class','Supply_Chain_Class')).rename(columns = {'Goods_Mvmt_Class':'Goods Movement Class','Supply_Chain_Class':'Supply Chain Class','EMPNUM':'Total Employment'})
-
-#Group DF by Goods Movement Class and sum employment 
-megaRegionEmpTotByGMClass2016 = norCalMegaRegBusinessesDF.groupby(['Goods Movement Class']).agg({'Total Employment':'sum'})
-
-#Add Percentage of Total Employment Column and calculate percentage of total employment
-megaRegionEmpTotByGMClass2016['Percentage of Total'] = megaRegionEmpTotByGMClass2016.apply(lambda x: 100 * x / float(x.sum()))
-
-#Export to CSV 
-megaRegionEmpTotByGMClass2016.to_csv(r"\\Mac\Home\Documents\Github_Documentation\Regional-Goods-Movement\Northern-California-Megaregion-Employment-Density-Maps\data\2016_Megaregion_Emp_Tot_By_GM_Class.csv")
-
-#Group DF by Supply Chain Class and sum employment
-megaRegionEmpTotBySCClass2016 = norCalMegaRegBusinessesDF.groupby(['Supply Chain Class']).agg({'Total Employment':'sum'}) 
-
-#Add Percentage of Total Employment Column and calculate percentage of total employment 
-megaRegionEmpTotBySCClass2016['Percentage of Total'] = megaRegionEmpTotBySCClass2016.apply(lambda x: 100 * x / float(x.sum()))
-
-#Export to CSV
-megaRegionEmpTotBySCClass2016.to_csv(r"\\Mac\Home\Documents\Github_Documentation\Regional-Goods-Movement\Northern-California-Megaregion-Employment-Density-Maps\data\2016_Megaregion_Emp_Tot_By_SC_Class.csv")
 
 
 
