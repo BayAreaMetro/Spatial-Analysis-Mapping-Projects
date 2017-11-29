cad658045e058200e155d1bb3911f5fd9f31c526
jcroff <joshua.croff@gmail.com>
2017-11-29 10:24:34 -0800

    create script to resize images in local directory

--- Project-Documentation/motm/utility_scripts/resize_image.py
+++ Project-Documentation/motm/utility_scripts/resize_image.py
@@ -2,18 +2,83 @@
 
 from PIL import Image
 from resizeimage import resizeimage
+import glob, os
 
-image_path = r"/Users/JoshCroff/Documents/Test_Images/motm_11_09_17_website.png"
-save_file = r"/Users/JoshCroff/Documents/Test_Images/motm_11_09_17_thumbnail.png"
+directory_path = r"/Users/jcroff/Box/MOTM_Uploads/motm_images/*.png"
 
-#fd_img = open(image_path, 'r')
-#img = Image.open(fd_img)
-#img = resizeimage.resize_cover(img, [600, 400])
-#img.save(save_file, img.format)
-#fd_img.close()
+def resize_and_crop(img_path, size, crop_type='top'):
+    """
+    Resize and crop an image to fit the specified size.
 
-fd_img = open(image_path, 'r')
-img = Image.open(fd_img)
-img = resizeimage.resize_thumbnail(img, [600, 400])
-img.save(save_file, img.format)
-fd_img.close()
+    args:
+    img_path: path for the image to resize.
+    size: `(width, height)` tuple.
+    crop_type: can be 'top', 'middle' or 'bottom', depending on this
+    value, the image will cropped getting the 'top/left', 'middle' or
+    'bottom/right' of the image to fit the size.
+    raises:
+    Exception: if can not open the file in img_path of there is problems
+    to save the image.
+    ValueError: if an invalid `crop_type` is provided.
+    """
+    # If height is higher we resize vertically, if not we resize horizontally
+    img = Image.open(img_path)
+    # Get current and desired ratio for the images
+    img_ratio = img.size[0] / float(img.size[1])
+    ratio = size[0] / float(size[1])
+    #The image is scaled/cropped vertically or horizontally depending on the ratio
+    if ratio > img_ratio:
+        img = img.resize((size[0], int(round(size[0] * img.size[1] / img.size[0]))),
+            Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, img.size[0], size[1])
+        elif crop_type == 'middle':
+            box = (0, int(round((img.size[1] - size[1]) / 2)), img.size[0],
+                int(round((img.size[1] + size[1]) / 2)))
+        elif crop_type == 'bottom':
+            box = (0, img.size[1] - size[1], img.size[0], img.size[1])
+        else :
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    elif ratio < img_ratio:
+        img = img.resize((int(round(size[1] * img.size[0] / img.size[1])), size[1]),
+            Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, size[0], img.size[1])
+        elif crop_type == 'middle':
+            box = (int(round((img.size[0] - size[0]) / 2)), 0,
+                int(round((img.size[0] + size[0]) / 2)), img.size[1])
+        elif crop_type == 'bottom':
+            box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
+        else :
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    else :
+        img = img.resize((size[0], size[1]),
+            Image.ANTIALIAS)
+    # If the scale is the same, we do not need to crop
+    file, ext = os.path.splitext(img_path)
+    img.save(file + "_thumbnail.png", "PNG")
+    #img.save(modified_path)
+
+#provide path to directory of images
+def resize_imgs_directory(size, dir_path, crop_type):
+    """
+    Resize and crop each image in a directory to fit a certain size 
+
+    args:
+    dir_path: path for directory of images to resize.
+    size: `(width, height)` tuple.
+    crop_type: can be 'top', 'middle' or 'bottom', depending on this
+    value, the image will cropped getting the 'top/left', 'middle' or
+    'bottom/right' of the image to fit the size. 
+    """
+    for img_path in glob.glob(dir_path):
+        resize_and_crop(img_path, size, crop_type)
+
+resize_imgs_directory((600,400), directory_path, crop_type='top')
+
+
+
